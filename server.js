6090dfd853b2fa55a6edbb805ea866f9c8293571
Amir Rad <ahmr5@yahoo.co.uk>
2019-11-30 18:27:06 +0000

    up and down vote

--- server.js
+++ server.js
@@ -4,8 +4,7 @@ const url = require('url');
 const fs = require('fs');
 
 const posts = [];
-
-
+const users = [];
 
 const app = express();
 
@@ -16,13 +15,37 @@ app.get('/posts', (req, res) => {
   res.json(posts)
 })
 
+app.post('/createUser', (req, res) => {
+  const obj = req.body;
+  obj.loggedIn = true;
+  users.push(obj);
+  res.send(200);
+})
+
+app.get('/downVote', (req, res) => {
+  const id = req.query.id
+  posts.filter(x => x.id === id).forEach(post => {
+    post.downVote++
+  })
+  res.send(200)
+})
+
+
+app.get('/upVote', (req, res) => {
+  const id = req.query.id
+  posts.filter(x => x.id === id).forEach(post => {
+    post.upVote++
+  })
+  res.send(200)
+})
+
 app.post('/newPost', (req, res) => {
   const obj = req.body
+  obj.upVote = 0;
+  obj.downVote = 0;
   posts.push(obj);
-  console.log(posts);
-  // const data = JSON.stringify(obj);
-  // fs.writeFileSync('news.json', data)
   res.send(200);
+  console.log(posts)
 })
 
 const port = process.argv[2] || 3000;
