b9242d7d487dbf152363a34c3b11596ef95a39e4
Amir Rad <ahmr5@yahoo.co.uk>
2019-11-30 15:53:30 +0000

    post request (newPost)

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const http = require('http');
 const url = require('url');
+const fs = require('fs');
 
 
 
@@ -8,6 +9,12 @@ const app = express();
 
 app.use(express.json());
 app.use('/', express.static('static'));
+
+app.post('/newPost', (req, res) => {
+  const obj = req.body
+  const data = JSON.stringify(obj);
+  fs.writeFileSync('news.json', data)
+})
 
 const port = process.argv[2] || 3000;
  
