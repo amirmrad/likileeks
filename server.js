80580b298a1dc7d699e3683802b59292e09a5ae3
Amir Rad <ahmr5@yahoo.co.uk>
2019-11-30 22:03:58 +0000

    restructre back-end

--- server.js
+++ server.js
@@ -1,103 +1,14 @@
 const express = require('express');
-const mongo = require('mongodb').MongoClient;
-const http = require('http');
-const url = require('url');
-const fs = require('fs');
-const dburl = 'mongodb://localhost:27017';
-
-// mongo.connect(dburl, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-//   }, (err, client) => {
-//   if (err) {
-//     console.error(err)
-//     return
-//   }
-//     const db = client.db('likileaks')
-//     const userCol = db.collection('user')
-//     userCol.insertOne({name: 'Amir', age: 24}, (err, result) => {
-//       if (err) {
-//         console.log(err)
-//       }
-//     })
-// })
-
-
-
-const posts = [];
-const users = [];
+const userRoute = require('./user')
+const postRoute = require('./post')
 
 const app = express();
 
 app.use(express.json());
+app.use('/user', userRoute);
+app.use('/post', postRoute);
 app.use('/', express.static('static'));
 
-app.get('/posts', (req, res) => {
-  res.json(posts)
-})
-
-app.post('/createUser', (req, res) => {
-  const obj = req.body;
-  // obj.loggedIn = true;
-  mongo.connect(dburl, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    }, (err, client) => {
-    if (err) {
-      console.error(err)
-      return
-    }
-      const db = client.db('likileaks')
-      const userCol = db.collection('user')
-      userCol.insertOne(obj, (err, result) => {
-        if (err) {
-          console.log(err)
-        }
-      })
-  })
-  res.send(200);
-})
-
-app.get('/downVote', (req, res) => {
-  const id = req.query.id
-  posts.filter(x => x.id === id).forEach(post => {
-    post.downVote++
-  })
-  res.send(200)
-})
-
-
-app.get('/upVote', (req, res) => {
-  const id = req.query.id
-  posts.filter(x => x.id === id).forEach(post => {
-    post.upVote++
-  })
-  res.send(200)
-})
-
-app.post('/newPost', (req, res) => {
-  const obj = req.body
-  obj.upVote = 0;
-  obj.downVote = 0;
-  mongo.connect(dburl, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    }, (err, client) => {
-    if (err) {
-      console.error(err)
-      return
-    }
-      const db = client.db('likileaks')
-      const userCol = db.collection('user')
-      userCol.insertOne(obj, (err, result) => {
-        if (err) {
-          console.log(err)
-        }
-      })
-  })
-  res.send(200);
-  console.log(posts)
-})
 
 const port = process.argv[2] || 3000;
  
