bbe88f88a82b6a729b6ae6f7b5d2e9a4ca093d76
“Amir <amir.hosseini@kcl.ac.uk>
2019-11-30 23:58:32 +0000

    GONA PUSH

--- server.js
+++ server.js
@@ -1,14 +1,66 @@
 const express = require('express');
-const userRoute = require('./user')
-const postRoute = require('./post')
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+
+const posts = [];
+const users = [];
+
 
 const app = express();
 
 app.use(express.json());
-app.use('/user', userRoute);
-app.use('/post', postRoute);
 app.use('/', express.static('static'));
 
+app.get('/posts', (req, res) => {
+  res.json(posts)
+})
+
+app.post('/createUser', (req, res) => {
+  const obj = req.body;
+  obj.loggedIn = true;
+  users.push(obj);
+  res.send(200);
+})
+
+app.get('/downVote', (req, res) => {
+  const id = req.query.id
+  posts.filter(x => x.id === id).forEach(post => {
+    post.downVote++
+  })
+  res.send(200)
+})
+
+
+app.get('/upVote', (req, res) => {
+  const id = req.query.id
+  posts.filter(x => x.id === id).forEach(post => {
+    post.upVote++
+  })
+  res.send(200)
+})
+
+app.post('/newPost', (req, res) => {
+  const obj = req.body
+  obj.upVote = 0;
+  obj.downVote = 0;
+  posts.push(obj);
+  res.send(200);
+  console.log(posts)
+})
+
+app.get('/users', (req, res) => {
+    res.json(users)
+})
+
+app.post('/newUser', (req, res) => {
+    const obj = req.body
+    posts.push(obj);
+    console.log(users);
+    // const data = JSON.stringify(obj);
+    // fs.writeFileSync('news.json', data)
+    res.send(200);
+})
 
 const port = process.argv[2] || 3000;
  
--- user.js
+++ user.js
@@ -26,4 +26,32 @@ router.post('/createUser', (req, res) => {
   res.send(200);
 })
 
+router.get('/getUser', (req, res) => {
+  const username = req.query.username;
+  mongo.connect(dburl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }, (err, client) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    const db = client.db('likileaks')
+    const userCol = db.collection('user')
+    userCol.findOne({username : username}, (err, result) => {
+      if (err) {
+        console.log(err)
+        res.send(404);
+      } else {
+        if (result) {
+          console.log(result);
+          res.json(result);
+        } else {
+          res.send(404);
+        }
+      }
+    })
+  })
+})
+
 module.exports = router
